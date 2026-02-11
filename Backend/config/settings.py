a933e0104a45f6360f0308e776065e0f8006be14
Ananya Visen <ananyavisen@gmail.com>
2026-02-12 03:54:36 +0530

    Revert to sqlite

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+
 import dj_database_url
 
 load_dotenv()
@@ -83,7 +84,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+   'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+}
 }
 
 
